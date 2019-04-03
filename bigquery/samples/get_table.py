dd2a22abab87d561cac0a4bc54a7f279c86159c1
Layla Bristol <laylab@google.com>
2019-04-03 15:26:47 -0700

    Corrected typo

--- bigquery/samples/get_table.py
+++ bigquery/samples/get_table.py
@@ -21,7 +21,7 @@ def get_table(client, table_id):
     # TODO(developer): Construct a BigQuery client object.
     # client = bigquery.Client()
 
-    # TODO(developer): Set model_id to the ID of the model to fetch.
+    # TODO(developer): Set table_id to the ID of the model to fetch.
     # table_id = 'your-project.your_dataset.your_table'
 
     table = client.get_table(table_id)
