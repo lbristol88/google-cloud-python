a30f103504c241b126fe3811f62bdfcab06726ca
Layla Bristol <laylab@google.com>
2019-04-03 16:40:10 -0700

    Applied changes based on feedback.

--- bigquery/samples/create_dataset.py
+++ bigquery/samples/create_dataset.py
@@ -14,16 +14,25 @@
 
 
 def create_dataset(client, dataset_id):
+
     # [START bigquery_create_dataset]
     from google.cloud import bigquery
 
     # TODO(developer): Construct a BigQuery client object.
     # client = bigquery.Client()
 
-    # TODO(developer): Set dataset_id to the ID of the dataset to create
-    # dataset_id = "your-project.your_dataset"
+    # TODO(developer): Set dataset_id to the ID of the dataset to create.
+    # dataset_id = "{}.your_dataset".format(client.project)
 
+    # Construct a full Dataset object to send to the API.
     dataset = bigquery.Dataset(dataset_id)
+
+    # TODO(developer): Specify the geographic location where the dataset should reside.
+    # dataset.location = "US"
+
+    # Send the dataset to the API for creation.
+    # Raises google.api_core.exceptions.Conflict if the Dataset already
+    # exists within the project.
     dataset = client.create_dataset(dataset)  # API request
     print("Created dataset {}.{}".format(client.project, dataset.dataset_id))
     # [END bigquery_create_dataset]
